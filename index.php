f5b9dc1df92f879914fd8c2f50f5189e46f9b1fd
Rajdi MUCANJI <108236895+Rajdi16@users.noreply.github.com>
2026-01-16 08:38:10 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- index.php
+++ index.php
@@ -523,7 +523,7 @@ foreach ($items as $item) {
       <div class="section-title">Folders</div>
       <div class="grid">
         <?php foreach ($folders as $f): ?>
-          <a href="<?php echo $f['path']; ?>" class="card search-item">
+          <a href="<?php echo $f['path']; ?>" class="card search-item" target="_blank">
             <div class="card-icon-box folder-box">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
